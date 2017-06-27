b8a28675568ab5d018800e003609cec7df81215d
econgamer <econgamer@gmail.com>
2017-06-27 17:36:24 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, function(err, salt){
+//   bcrypt.hash(password, salt, function(err, hash){
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$BvZIuebDot0bPlK6syRyTOn8FmXmktxHphRDMW0Q5pOaO3JTTwrLC';
+
+bcrypt.compare('password', hashedPassword, function(err, res){
+  console.log(res);
+});
+
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 
